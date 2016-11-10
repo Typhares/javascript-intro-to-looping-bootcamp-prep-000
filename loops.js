d130df0ea9338b42b0d36a6dfd1c2f3731f364c7
Elizabeth Hall <elizabethhall205@gmail.com>
2016-11-10 18:13:56 +0000

    Done.

--- loops.js
+++ loops.js
@@ -9,3 +9,22 @@ function forLoop(array) {
   }
   return array
 }
+
+function whileLoop(n) {
+  while(n > 0 ) {
+    console.log(--n)
+  }
+  return 'done'
+}
+
+function doWhileLoop(array){
+  function maybeTrue() {
+    return Math.random() >= 0.5
+    }
+  do {
+    array.shift()
+
+  }  while(array.length > 0 && maybeTrue());
+
+  return array
+}
